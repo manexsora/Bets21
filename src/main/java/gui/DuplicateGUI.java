8622af2e778c64a9d8decb66a12e9278af9cb5fd
ansuehu <98747387+ansuehu@users.noreply.github.com>
2022-05-10 19:43:52 +0200

    Add files via upload

---
+++ src/main/java/gui/DuplicateGUI.java
@@ -0,0 +1,220 @@
+package gui;
+
+import businessLogic.BLFacade;
+import configuration.UtilDate;
+
+import com.toedter.calendar.JCalendar;
+import domain.Question;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import javax.swing.table.DefaultTableModel;
+
+
+public class DuplicateGUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private final JLabel jLabelEventDate = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("EventDate"));
+	private final JLabel jLabelEvents = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("Event")); 
+	private JLabel jLabelMsg = new JLabel();
+
+	// Code for JCalendar
+	private JCalendar jCalendar1 = new JCalendar();
+	private Calendar calendarAnt = null;
+	private Calendar calendarAct = null;
+	private JScrollPane scrollPaneEvents = new JScrollPane();
+	
+	
+	private Vector<Date> datesWithEventsCurrentMonth = new Vector<Date>();
+
+	private JTable tableEvents= new JTable();
+
+	private DefaultTableModel tableModelEvents;
+	private DefaultTableModel tableModelQueries;
+
+	
+	private String[] columnNamesEvents = new String[] {
+			ResourceBundle.getBundle("Etiquetas").getString("EventN"), 
+			ResourceBundle.getBundle("Etiquetas").getString("Events")
+
+	};
+	private String[] columnNamesQueries = new String[] {
+			ResourceBundle.getBundle("Etiquetas").getString("QueryN"), 
+			ResourceBundle.getBundle("Etiquetas").getString("Query")
+		
+
+	};
+
+	public DuplicateGUI(Event ev)
+	{
+		try
+		{
+			jbInit(ev);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	
+	private void jbInit(Event ev) throws Exception
+	{
+
+		this.getContentPane().setLayout(null);
+		this.setSize(new Dimension(666, 335));
+		this.setTitle(ResourceBundle.getBundle("Etiquetas").getString("CreateEvent"));
+
+		jLabelEventDate.setBounds(new Rectangle(40, 15, 140, 25));
+		jLabelEvents.setBounds(295, 19, 259, 16);
+
+		this.getContentPane().add(jLabelEventDate, null);
+		this.getContentPane().add(jLabelEvents);
+
+
+		jCalendar1.setBounds(new Rectangle(40, 50, 225, 150));
+
+		BLFacade facade = AdminGUI.getBusinessLogic();
+		datesWithEventsCurrentMonth=facade.getEventsMonth(jCalendar1.getDate());
+		CreateQuestionGUI.paintDaysWithEvents(jCalendar1,datesWithEventsCurrentMonth);
+
+		// Code for JCalendar
+		this.jCalendar1.addPropertyChangeListener(new PropertyChangeListener()
+		{
+			public void propertyChange(PropertyChangeEvent propertychangeevent)
+			{
+
+				if (propertychangeevent.getPropertyName().equals("locale"))
+				{
+					jCalendar1.setLocale((Locale) propertychangeevent.getNewValue());
+				}
+				else if (propertychangeevent.getPropertyName().equals("calendar"))
+				{
+					calendarAnt = (Calendar) propertychangeevent.getOldValue();
+					calendarAct = (Calendar) propertychangeevent.getNewValue();
+					DateFormat dateformat1 = DateFormat.getDateInstance(1, jCalendar1.getLocale());
+//					jCalendar1.setCalendar(calendarAct);
+					Date firstDay=UtilDate.trim(new Date(jCalendar1.getCalendar().getTime().getTime()));
+
+					 
+					
+					int monthAnt = calendarAnt.get(Calendar.MONTH);
+					int monthAct = calendarAct.get(Calendar.MONTH);
+					
+					if (monthAct!=monthAnt) {
+						if (monthAct==monthAnt+2) {
+							// Si en JCalendar está 30 de enero y se avanza al mes siguiente, devolvería 2 de marzo (se toma como equivalente a 30 de febrero)
+							// Con este código se dejará como 1 de febrero en el JCalendar
+							calendarAct.set(Calendar.MONTH, monthAnt+1);
+							calendarAct.set(Calendar.DAY_OF_MONTH, 1);
+						}						
+						
+						jCalendar1.setCalendar(calendarAct);
+
+						BLFacade facade = AdminGUI.getBusinessLogic();
+
+						datesWithEventsCurrentMonth=facade.getEventsMonth(jCalendar1.getDate());
+					}
+
+
+
+					CreateQuestionGUI.paintDaysWithEvents(jCalendar1,datesWithEventsCurrentMonth);
+													
+					
+
+					try {
+						tableModelEvents.setDataVector(null, columnNamesEvents);
+						tableModelEvents.setColumnCount(3); // another column added to allocate ev objects
+
+						BLFacade facade=AdminGUI.getBusinessLogic();
+
+						Vector<domain.Event> events=facade.getEvents(firstDay);
+
+						if (events.isEmpty() ) jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("NoEvents")+ ": "+dateformat1.format(calendarAct.getTime()));
+						else jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("Events")+ ": "+dateformat1.format(calendarAct.getTime()));
+						for (domain.Event ev:events){
+							Vector<Object> row = new Vector<Object>();
+
+							System.out.println("Events "+ev);
+
+							row.add(ev.getEventNumber());
+							row.add(ev.getDescription());
+							row.add(ev); // ev object added in order to obtain it with tableModelEvents.getValueAt(i,2)
+							tableModelEvents.addRow(row);		
+						}
+						tableEvents.getColumnModel().getColumn(0).setPreferredWidth(25);
+						tableEvents.getColumnModel().getColumn(1).setPreferredWidth(268);
+						tableEvents.getColumnModel().removeColumn(tableEvents.getColumnModel().getColumn(2)); // not shown in JTable
+					} catch (Exception e1) {
+
+						
+					}
+
+				}
+			} 
+		});
+
+		this.getContentPane().add(jCalendar1, null);
+		
+		scrollPaneEvents.setBounds(new Rectangle(292, 50, 346, 150));
+
+		tableEvents.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i=tableEvents.getSelectedRow();
+				domain.Event ev=(domain.Event)tableModelEvents.getValueAt(i,2); // obtain ev object
+				Vector<Question> queries=ev.getQuestions();
+
+				tableModelQueries.setDataVector(null, columnNamesQueries);
+
+
+				for (domain.Question q:queries){
+					Vector<Object> row = new Vector<Object>();
+
+					row.add(q.getQuestionNumber());
+					row.add(q.getQuestion());
+					tableModelQueries.addRow(row);	
+				}
+
+			}
+		});
+
+		scrollPaneEvents.setViewportView(tableEvents);
+		tableModelEvents = new DefaultTableModel(null, columnNamesEvents);
+
+		tableEvents.setModel(tableModelEvents);
+		tableEvents.getColumnModel().getColumn(0).setPreferredWidth(25);
+		tableEvents.getColumnModel().getColumn(1).setPreferredWidth(268);
+		tableModelQueries = new DefaultTableModel(null, columnNamesQueries);
+
+		this.getContentPane().add(scrollPaneEvents, null);
+		
+		
+		jLabelMsg.setBounds(392, 226, 208, 31);
+		getContentPane().add(jLabelMsg);
+		
+		JButton btnDuplicateEvent = new JButton(ResourceBundle.getBundle("Etiquetas").getString("DeleteEvent"));
+		btnDuplicateEvent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(jCalendar1.getDate()!=null) {	
+					jLabelMsg.setText(ResourceBundle.getBundle("Etiquetas").getString("NoEventSelected"));
+				}
+				else {
+					Date d = jCalendar1.getDate();
+					facade.duplicate(ev, d);
+				}
+				
+			}
+		});
+		btnDuplicateEvent.setBounds(new Rectangle(98, 420, 130, 30));
+		btnDuplicateEvent.setBounds(229, 244, 179, 30);
+		getContentPane().add(btnDuplicateEvent);
+
+	}
+}
