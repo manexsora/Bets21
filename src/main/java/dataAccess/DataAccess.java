5acffc127fdcf42038cb6d0edea063aac1d8d70c
manexsora <98772019+manexsora@users.noreply.github.com>
2022-05-14 01:23:48 +0200

    Update DataAccess.java

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -69,27 +69,27 @@ public class DataAccess  {
 			int year=today.get(Calendar.YEAR);
 			if (month==12) { month=0; year+=1;}  
 
-			Event ev1=new Event(1, "Atlético-Athletic", UtilDate.newDate(year,month,17));
+			Event ev1=new Event(1, "AtlÃ©tico-Athletic", UtilDate.newDate(year,month,17));
 			Event ev2=new Event(2, "Eibar-Barcelona", UtilDate.newDate(year,month,17));
 			Event ev3=new Event(3, "Getafe-Celta", UtilDate.newDate(year,month,17));
-			Event ev4=new Event(4, "Alavés-Deportivo", UtilDate.newDate(year,month,17));
-			Event ev5=new Event(5, "Español-Villareal", UtilDate.newDate(year,month,17));
+			Event ev4=new Event(4, "AlavÃ©s-Deportivo", UtilDate.newDate(year,month,17));
+			Event ev5=new Event(5, "EspaÃ±ol-Villareal", UtilDate.newDate(year,month,17));
 			Event ev6=new Event(6, "Las Palmas-Sevilla", UtilDate.newDate(year,month,17));
 			Event ev7=new Event(7, "Malaga-Valencia", UtilDate.newDate(year,month,17));
-			Event ev8=new Event(8, "Girona-Leganés", UtilDate.newDate(year,month,17));
+			Event ev8=new Event(8, "Girona-LeganÃ©s", UtilDate.newDate(year,month,17));
 			Event ev9=new Event(9, "Real Sociedad-Levante", UtilDate.newDate(year,month,17));
 			Event ev10=new Event(10, "Betis-Real Madrid", UtilDate.newDate(year,month,17));
 
 			Event ev11=new Event(11, "Atletico-Athletic", UtilDate.newDate(year,month,1));
 			Event ev12=new Event(12, "Eibar-Barcelona", UtilDate.newDate(year,month,1));
 			Event ev13=new Event(13, "Getafe-Celta", UtilDate.newDate(year,month,1));
-			Event ev14=new Event(14, "Alavés-Deportivo", UtilDate.newDate(year,month,1));
-			Event ev15=new Event(15, "Español-Villareal", UtilDate.newDate(year,month,1));
+			Event ev14=new Event(14, "AlavÃ©s-Deportivo", UtilDate.newDate(year,month,1));
+			Event ev15=new Event(15, "EspaÃ±ol-Villareal", UtilDate.newDate(year,month,1));
 			Event ev16=new Event(16, "Las Palmas-Sevilla", UtilDate.newDate(year,month,1));
 
 
-			Event ev17=new Event(17, "Málaga-Valencia", UtilDate.newDate(year,month+1,28));
-			Event ev18=new Event(18, "Girona-Leganés", UtilDate.newDate(year,month+1,28));
+			Event ev17=new Event(17, "MÃ¡laga-Valencia", UtilDate.newDate(year,month+1,28));
+			Event ev18=new Event(18, "Girona-LeganÃ©s", UtilDate.newDate(year,month+1,28));
 			Event ev19=new Event(19, "Real Sociedad-Levante", UtilDate.newDate(year,month+1,28));
 			Event ev20=new Event(20, "Betis-Real Madrid", UtilDate.newDate(year,month+1,28));
 
@@ -101,12 +101,12 @@ public class DataAccess  {
 			Question q6;
 
 			if (Locale.getDefault().equals(new Locale("es"))) {
-				q1=ev1.addQuestion("¿Quién ganará el partido?",1 );
-				q2=ev1.addQuestion("¿Quién meterá el primer gol?",2 );
-				q3=ev11.addQuestion("¿Quién ganará el partido?",1 );
-				q4=ev11.addQuestion("¿Cuántos goles se marcarán?",2 );
-				q5=ev17.addQuestion("¿Quién ganará el partido?",1 );
-				q6=ev17.addQuestion("¿Habrá goles en la primera parte?",2 );
+				q1=ev1.addQuestion("Â¿QuiÃ©n ganarÃ¡ el partido?",1 );
+				q2=ev1.addQuestion("Â¿QuiÃ©n meterÃ¡ el primer gol?",2 );
+				q3=ev11.addQuestion("Â¿QuiÃ©n ganarÃ¡ el partido?",1 );
+				q4=ev11.addQuestion("Â¿CuÃ¡ntos goles se marcarÃ¡n?",2 );
+				q5=ev17.addQuestion("Â¿QuiÃ©n ganarÃ¡ el partido?",1 );
+				q6=ev17.addQuestion("Â¿HabrÃ¡ goles en la primera parte?",2 );
 			}
 			else if (Locale.getDefault().equals(new Locale("en"))) {
 				q1=ev1.addQuestion("Who will win the match?",1 );
@@ -320,7 +320,7 @@ public class DataAccess  {
 	public boolean isRegister(String user) {
 
 
-		User a = db.find(User.class, user);
+		User a = db.find(Registered.class, user);
 		if(a==null) {
 			return false;
 		}
